f170feb6bafba260a088766a9f492cd85c660c3d
uhyunjin <uhyeonjin0619@gmail.com>
2025-01-06 18:15:53 +0900

    querydsl을 사용한 paging 테스트

---
+++ src/main/java/com/example/b01/repository/search/BoardSearchImpl.java
@@ -0,0 +1,68 @@
+package com.example.b01.repository.search;
+
+import com.example.b01.domain.Board;
+import com.example.b01.domain.QBoard;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.JPQLQuery;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.Querydsl;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.List;
+
+public class BoardSearchImpl extends QuerydslRepositorySupport implements BoardSearch {
+    public BoardSearchImpl() {
+        super(Board.class);
+    }
+    @Override
+    public Page<Board> search1(Pageable pageable) {
+        //타입 기반으로 코드를 이용해서 jpql 쿼리를 생성 및 실행한다 : Type Safety
+        //코드를 만드는 대신 클래스가 Q도메인 클래스!!
+        QBoard board = QBoard.board;    //Q도메인 객체
+        JPQLQuery<Board> query = from(board);   //select...from board
+        query.where(board.title.contains("1")); //where title like...
+
+        //paging
+        this.getQuerydsl().applyPagination(pageable, query);
+
+        List<Board> list = query.fetch();
+        long count = query.fetchCount();
+        return null;
+    }
+
+    @Override
+    public Page<Board> searchAll(String[] types, String keyword, Pageable pageable) {
+        QBoard board = QBoard.board;
+        JPQLQuery<Board> query = from(board);
+
+        if ((types!=null&&types.length>0)&&keyword!=null){
+            BooleanBuilder booleanBuilder = new BooleanBuilder();
+            for (String type : types) {
+                switch (type) {
+                    case "t":
+                        booleanBuilder.or(board.title.contains("keyword"));
+                        break;
+                    case "c":
+                        booleanBuilder.or(board.content.contains("keyword"));
+                        break;
+                    case "w":
+                        booleanBuilder.or(board.writer.contains("keyword"));
+                        break;
+                }
+            }
+            query.where(booleanBuilder);    //if문 실행 시 and 조건으로 추가
+        }
+        // bno>0
+        query.where(board.bno.gt(0L));
+
+        //paging
+        this.getQuerydsl().applyPagination(pageable, query);
+        List<Board> list = query.fetch();
+        long count = query.fetchCount();
+
+        return null;
+    }
+
+
+}
--- src/test/java/com/example/b01/repository/BoardRepositoryTests.java
+++ src/test/java/com/example/b01/repository/BoardRepositoryTests.java
@@ -5,7 +5,12 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -15,29 +20,65 @@ public class BoardRepositoryTests {
     @Autowired
     private BoardRepository boardRepository;
 
-    @Test
-    public void testInsert(){
-        IntStream.rangeClosed(1,100).forEach(i->{
-            Board board = Board.builder()
-                    .title("title..." + i)
-                    .content("content..." + i)
-                    .writer("user..." + i)
-                    .build();
-            Board result = boardRepository.save(board);
-            log.info("BNO : " + result.getBno());
-        });
-    }
+//    @Test
+//    public void testInsert() {
+//        IntStream.rangeClosed(1, 100).forEach(i -> {
+//            Board board = Board.builder()
+//                    .title("title..." + i)
+//                    .content("content..." + i)
+//                    .writer("user..." + i)
+//                    .build();
+//            Board result = boardRepository.save(board);
+//            log.info("BNO : " + result.getBno());
+//        });
+//    }
+//
+//    @Test
+//    public void testSelect() {
+//        Long bno = 99L;
+//        Optional<Board> result = boardRepository.findById(bno);
+//        Board board = result.orElseThrow();
+//        log.info(board);
+//    }
+
+//    @Test
+//    public void testUpdate() {
+//        Long bno=100L;
+//        Optional<Board> result = boardRepository.findById(bno);
+//        Board board = result.orElseThrow();
+//        board.change("update..title 100","update content 100");
+//        boardRepository.save(board);
+//    }
+
+//    @Test
+//    public void testDelete() {
+//        Long bno = 100L;
+//        boardRepository.deleteById(bno);
+//    }
+//
+//    @Test
+//    public void testPaging() {
+//        Pageable pageble = PageRequest.of(0, 10, Sort.by("bno").descending());
+//        Page<Board> result = boardRepository.findAll(pageble);
+//        log.info("total count : " + result.getTotalElements());   // 전체 데이터의 개수
+//        log.info("total pages : " + result.getTotalPages());
+//        log.info("page size :  " + result.getSize());
+//        log.info("page number : " + result.getNumber());  // 현재 페이지의 번호 0부터 시작
+//        List<Board> todoList = result.getContent();
+//        todoList.forEach(board -> log.info(board));
+//    }
+//
+//    @Test
+//    public void testSearch() {
+//        Pageable pageable = PageRequest.of(1, 10, Sort.by("bno").descending());
+//        boardRepository.search1(pageable);
+//    }
 
     @Test
-    public void testSelect() {
-        Long bno = 99L;
-        Optional<Board> result = boardRepository.findById(bno);
-        Board board = result.orElseThrow();
-        log.info(board);
-    }
-
-    @Test
-    public void testUpdate() {
-
+    public void testSearchAll() {
+        String[] types = {"t", "c", "w"};
+        String keyword = "1";
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+        Page<Board> result = boardRepository.searchAll(types, keyword, pageable);
     }
 }
