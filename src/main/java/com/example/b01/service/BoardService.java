d3af44ee8224a5da155d8c1a4e3f9505d5124be0
uhyunjin <uhyeonjin0619@gmail.com>
2025-02-08 00:43:09 +0900

    [서비스] 게시물 읽기/수정 메서드 작성

--- src/main/java/com/example/b01/service/BoardService.java
+++ src/main/java/com/example/b01/service/BoardService.java
@@ -1,7 +1,12 @@
 package com.example.b01.service;
 
+import com.example.b01.domain.Board;
 import com.example.b01.dto.BoardDTO;
 
 public interface BoardService {
     Long register(BoardDTO boardDTO);
+
+    BoardDTO readOne(Long bno);
+
+    void modify(BoardDTO boardDTO);
 }
--- src/main/java/com/example/b01/service/BoardServiceImpl.java
+++ src/main/java/com/example/b01/service/BoardServiceImpl.java
@@ -11,6 +11,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 @Service
 @Log4j2
 @RequiredArgsConstructor
@@ -24,5 +27,22 @@ public class BoardServiceImpl implements BoardService {
         Board board = modelMapper.map(boardDTO, Board.class);
         Long bno = boardRepository.save(board).getBno();
         return bno;
+    }
+
+    @Override
+    public BoardDTO readOne(Long bno) {
+        Optional<Board> result = boardRepository.findById(bno);
+        Board board = result.orElseThrow();
+        BoardDTO boardDTO = modelMapper.map(board, BoardDTO.class);
+
+        return boardDTO;
+    }
+
+    @Override
+    public void modify(BoardDTO boardDTO) {
+        Optional<Board> result = boardRepository.findById(boardDTO.getBno());
+        Board board = result.orElseThrow();
+        board.change(boardDTO.getTitle(), boardDTO.getContent());
+        boardRepository.save(board);
     }
 }
